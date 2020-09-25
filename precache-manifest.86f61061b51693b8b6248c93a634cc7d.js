688054e07df7b0bdf6d880bf0816d7ba8feabcae
FrikanRw <ferwee@gmail.com>
2020-09-25 11:04:47 +0200

    Updates

---
+++ precache-manifest.86f61061b51693b8b6248c93a634cc7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd2c9cf08a1efb40d2ea8fdeae7c413",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ff52f293ac6c2d6ce76",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "b803635795b28de17612",
+    "url": "./static/js/2.0d5205b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0d5205b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff52f293ac6c2d6ce76",
+    "url": "./static/js/main.c0dc708f.chunk.js"
+  },
+  {
+    "revision": "bb78c203f63959f74893",
+    "url": "./static/js/runtime-main.89d030b1.js"
+  }
+]);
