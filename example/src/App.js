34b5809f9f8fdaca0ba190d24a56c6f7e1386492
Werner Potgieter <wpotgid@gmail.com>
2020-10-04 12:57:01 +0200

    Added Prop Types
    NEW PACKAGE ADDED: prop-types
    
    Added propTypes to the component to show how the data needs to look like for it to render correctly.
    
    Added a forward ref for the component  as well.

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,26 @@
-import React, {useEffect, useState} from 'react'
-import * as d3 from 'd3';
-import { OrganisationalChart } from 'react-hierarchy-org-chart'
+import React, { useEffect, useRef, useState } from 'react'
+import * as d3 from 'd3'
+import OrganizationalChart from 'react-hierarchy-org-chart'
 import 'react-hierarchy-org-chart/dist/index.css'
 
 const App = () => {
-const [state, setstate] = useState(null)
+  const [state, setstate] = useState(null)
+  const d3Container = useRef(null)
   useEffect(() => {
     d3.json(
-      'https://gist.githubusercontent.com/bumbeishvili/dc0d47bc95ef359fdc75b63cd65edaf2/raw/c33a3a1ef4ba927e3e92b81600c8c6ada345c64b/orgChart.json',
-    ).then(data => {
-      setstate(data);
-    });
-  }, []);
+      'https://gist.githubusercontent.com/bumbeishvili/dc0d47bc95ef359fdc75b63cd65edaf2/raw/c33a3a1ef4ba927e3e92b81600c8c6ada345c64b/orgChart.json'
+    ).then((data) => {
+      setstate(data)
+    })
+  }, [])
 
-
-  return <OrganisationalChart 
-  data={state}
-  onNodeClick={node=>console.log(node)} />
+  return (
+    <OrganizationalChart
+      data={state}
+      ref={d3Container}
+      onNodeClick={(node) => console.log(node)}
+    />
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import TreeChart from './chartHelper/index'
-import styles from './styles.module.css'
+import './styles.module.css'
 
-export const OrganisationalChart = ({ data, onNodeClick }) => {
-  const d3Container = useRef(null)
+const OrganizationalChart = React.forwardRef(({ data, onNodeClick }, ref) => {
+  const d3Container = ref
   const [chart, setChart] = useState(null)
   const [id, setId] = useState('O-498')
 
@@ -31,7 +32,6 @@ export const OrganisationalChart = ({ data, onNodeClick }) => {
   }, [data, d3Container.current])
   if (chart) {
     console.log(chart)
-    // chart.locate('O-917')
   }
   return (
     <div>
@@ -45,4 +45,67 @@ export const OrganisationalChart = ({ data, onNodeClick }) => {
       <button onClick={() => chart.locate(id)}>Locate</button>
     </div>
   )
+})
+
+OrganizationalChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      backgroundColor: PropTypes.shape({
+        alpha: PropTypes.number,
+        red: PropTypes.number,
+        blue: PropTypes.number,
+        green: PropTypes.number
+      }),
+      borderColor: PropTypes.shape({
+        alpha: PropTypes.number,
+        red: PropTypes.number,
+        blue: PropTypes.number,
+        green: PropTypes.number
+      }),
+      borderRadius: PropTypes.number,
+      borderWidth: PropTypes.number,
+      connectorLineColor: PropTypes.shape({
+        alpha: PropTypes.number,
+        red: PropTypes.number,
+        blue: PropTypes.number,
+        green: PropTypes.number
+      }),
+      connectorLineWidth: PropTypes.number,
+      dashArray: PropTypes.string,
+      directSubordinates: PropTypes.number,
+      expanded: PropTypes.bool,
+      height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      nodeIcon: PropTypes.shape({
+        icon: PropTypes.string,
+        size: PropTypes.number
+      }),
+      nodeId: PropTypes.string.isRequired,
+      nodeImage: PropTypes.shape({
+        borderColor: PropTypes.shape({
+          alpha: PropTypes.number,
+          red: PropTypes.number,
+          blue: PropTypes.number,
+          green: PropTypes.number
+        }),
+        borderWidth: PropTypes.number,
+        centerLeftDistance: PropTypes.number,
+        centerTopDistance: PropTypes.number,
+        cornerShape: PropTypes.oneOf(['circle', 'rounded']),
+        height: PropTypes.number,
+        shadow: PropTypes.bool,
+        url: PropTypes.string,
+        width: PropTypes.number
+      }),
+      parentNodeId: PropTypes.string,
+      template: PropTypes.string,
+      totalSubordinates: PropTypes.number,
+      width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    })
+  ).isRequired,
+  /**
+   * A trigger to do a custom action when a node is clicked on
+   */
+  onNodeClick: PropTypes.func
 }
+
+export default OrganizationalChart
